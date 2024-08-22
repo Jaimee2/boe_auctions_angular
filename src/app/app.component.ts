377011d5a30c2ef14106c37959937073ea441547
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-08-22 08:24:08 +0200

    map init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {MapComponent} from "./map/map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterOutlet, MapComponent],
+  template:
+    `
+      <h1>Hello world</h1>
+
+      <div class="map-container">
+        <app-map></app-map>
+      </div>
+    `,
+  styles: [`
+    .map-container {
+      width: 80vh;
+      height: 80vh;
+      border: solid;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'boeAuctionAngularProject';
+
 }
