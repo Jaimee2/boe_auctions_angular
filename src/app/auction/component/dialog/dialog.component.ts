7df90d6c076531c580e3ff626573baa946fd4af4
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-08-29 10:57:13 +0200

    dialog

--- src/app/auction/component/dialog/dialog.component.ts
+++ src/app/auction/component/dialog/dialog.component.ts
@@ -7,11 +7,12 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
   standalone: true,
   imports: [],
   template: `
-    <div class="p-6 bg-white rounded-lg shadow-lg">
-      <h1 class="text-2xl font-bold text-gray-800 mb-4">Auction Details</h1>
+    <div class="p-4 bg-white rounded-lg shadow-lg max-w-md mx-auto">
+      <h1 class="text-xl font-bold text-gray-800 mb-3">Auction Details</h1>
 
-      <div class="mb-4">
-        <h2 class="text-2xl font-bold text-gray-800">Auction Information</h2>
+      <div class="mb-3">
+        <h2 class="text-lg font-semibold text-gray-700">Auction Information</h2>
+
         <p class="text-gray-600"><strong>Identifier:</strong> {{ data[0].identifier }}</p>
         <p class="text-gray-600"><strong>Start Date:</strong> {{ data[0].startDate }}</p>
         <p class="text-gray-600"><strong>End Date:</strong> {{ data[0].endDate }}</p>
@@ -24,8 +25,9 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
         <p class="text-gray-600"><strong>Deposit Amount:</strong> {{ data[0].depositAmount }}</p>
       </div>
 
-      <div class="mb-4">
-        <h2 class="text-xl font-semibold text-gray-700">Asset Information</h2>
+      <div class="mb-3">
+        <h2 class="text-lg font-semibold text-gray-700">Asset Information</h2>
+
         <p class="text-gray-600"><strong>Cadastral Reference:</strong> {{ data[1].cadastralReference }}</p>
         <p class="text-gray-600"><strong>Address:</strong> {{ data[1].fullAddress }}</p>
         <p class="text-gray-600"><strong>Coordinates:</strong> {{ data[1].coordinates.lat }}, {{ data[1].coordinates.lon }}</p>
@@ -37,14 +39,15 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
         <p class="text-gray-600"><strong>Primary Residence:</strong> {{ data[1].primaryResidence }}</p>
       </div>
 
-      <div class="mt-6">
+      <div class="mt-4 text-center">
         <button
           (click)="goToAssetPage()"
-          class="px-4 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
+          class="px-3 py-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700">
           Go to Asset Page
         </button>
       </div>
     </div>
+
   `,
   styles: ``
 })
