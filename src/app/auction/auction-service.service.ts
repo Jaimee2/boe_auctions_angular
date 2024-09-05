1499552725a27f914e2f30bb6265be7cfb8d72ce
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-09-05 16:21:47 +0200

    filter

--- src/app/auction/auction-service.service.ts
+++ src/app/auction/auction-service.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Auction} from "./interface/auction";
+import {AssetType} from "./constants";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import {Auction} from "./interface/auction";
 export class AuctionServiceService {
 
   private apiUrl = 'https://azure-function-boe-auction.azurewebsites.net/api';
+  assetTypes = [AssetType.Garaje, AssetType.Vivienda, AssetType.LocalComercial, AssetType.NaveIndustrial];
 
   constructor(private http: HttpClient) {
   }
@@ -17,10 +19,12 @@ export class AuctionServiceService {
     let params = new HttpParams();
     if (filters) {
       if (filters.province) params = params.set('province', filters.province);
-      //   this.assetTypes.forEach(type => {
-      //     if (filters[type]) params = params.append('assetType', type);
-      //   });
+
+        this.assetTypes.forEach(type => {
+          if (filters[type]) params = params.append('assetType', type);
+        });
     }
+
     return this.http.get<Auction[]>(`${this.apiUrl}/auctions`, {params});
   }
 
